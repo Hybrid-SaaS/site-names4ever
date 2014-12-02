a672eaa5b79d72e3ebc6d8af16052837c5d4d631
Randydedecker <randydedecker@gmail.com>
2014-12-02 11:39:36 +0100

    Console log weer verwijderd

--- pages/betaalpagina/betaalpagina.js
+++ pages/betaalpagina/betaalpagina.js
@@ -24,8 +24,6 @@ $.getJSON('/data/order/' + getParameterByName('order') + '/' + getParameterByNam
     }
     ga('ecommerce:send');
 
-    console.log(orderdata);
-
     // Beslist Nederland
     beslistQueue = [];
     beslistQueue.push(['setShopId', '3JHCC39SN']);
