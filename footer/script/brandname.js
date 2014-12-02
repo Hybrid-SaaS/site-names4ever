9f93a87b7fbd5ded6c721f2609381389ec3ef70c
Randydedecker <randydedecker@gmail.com>
2014-12-02 08:57:52 +0100

    Footer - Namesforever -> Names4ever in AT en CH

---
+++ footer/script/brandname.js
@@ -0,0 +1,6 @@
+﻿/// <reference path="../../definition/jquery.d.ts" />
+$(function () {
+    if (WebPage.Data.country == 'at | ch') {
+        $('#brandname-footer span').html('Names4ever');
+    }
+});
---
+++ footer/script/brandname.ts
@@ -0,0 +1,8 @@
+﻿/// <reference path="../../definition/jquery.d.ts" />
+
+
+$(() => {
+    if (WebPage.Data.country == 'at | ch') {
+        $('#brandname-footer span').html('Names4ever');
+    }
+});
