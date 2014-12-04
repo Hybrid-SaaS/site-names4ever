84b5d5173ec4ac40ba5f1985333c539101b754e6
Randydedecker <randydedecker@gmail.com>
2014-12-04 15:40:17 +0100

    Kerstbanner homepage

--- footer/script/brandname.js
+++ footer/script/brandname.js
@@ -1,7 +1,6 @@
 ﻿/// <reference path="../../definition/jquery.d.ts" />
-$(function () {
+(function () {
     if (WebPage.Data.country == 'at | ch') {
         $('#brandname-footer span').replaceWith('<span>Names4ever</span>');
     }
 });
-;
--- footer/script/brandname.ts
+++ footer/script/brandname.ts
@@ -1,7 +1,7 @@
 ﻿/// <reference path="../../definition/jquery.d.ts" />
 
 
-$(() => {
+(() => {
     if (WebPage.Data.country == 'at | ch') {
         $('#brandname-footer span').replaceWith('<span>Names4ever</span>');
     }
