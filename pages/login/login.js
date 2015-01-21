634ec3db35fc0ba08ab205cd0680c28bde0fdbca
Rick Littel <rick@hybridsaas.com>
2015-01-21 13:48:57 +0100

    Login script update

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,3 +1,16 @@
 /// <reference path="../../definition/jquery.d.ts" />
 $(function () {
+    $("#submitLogin").on("click", function () {
+        $.ajax({
+            type: 'POST',
+            dataType: 'json',
+            url: 'test.html',
+            data: {
+                "username": $("#usernameLogin input").val(),
+                "password": $("#passwordLogin input").val()
+            }
+        }).done(function (data) {
+            //Succes!
+        });
+    });
 });
--- pages/login/login.ts
+++ pages/login/login.ts
@@ -1,5 +1,17 @@
 /// <reference path="../../definition/jquery.d.ts" />
 
 $(function () {
-
+	$("#submitLogin").on("click", () => {
+		$.ajax({
+			type: 'POST',
+			dataType: 'json',
+			url: 'test.html',
+			data: {
+				"username": $("#usernameLogin input").val(),
+				"password": $("#passwordLogin input").val()
+			}
+		}).done(data => {
+			//Succes!
+		});
+	});
 });
