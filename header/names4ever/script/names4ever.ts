9acd325a789f5a5387e3ddc8734ec30baeb73e0c
Randydedecker <randydedecker@gmail.com>
2015-06-12 13:30:36 +0200

    v3

--- header/names4ever/script/names4ever.ts
+++ header/names4ever/script/names4ever.ts
@@ -967,7 +967,7 @@ $(function(){
     $searchFinder.on("click", () => 
 
     {
-        location.href = "/search?q=" + $searchTextBox.val().replace(/%20' '/g, '+');
+        location.href = "/search?q=" + $searchTextBox.val().replace(/\s/g, '+');
                 if (!isSearching) {
                     var searchValue = $searchTextBox.val();
 
