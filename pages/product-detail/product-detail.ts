9dc14a938270e7cc7c8dfd65b227baf30331890b
Randydedecker <randydedecker@gmail.com>
2015-06-24 09:30:08 +0200

    popup weg

--- pages/product-detail/product-detail.ts
+++ pages/product-detail/product-detail.ts
@@ -2,7 +2,7 @@
 /// <reference path="../../../../../Hybrid SaaS/Hybrid SaaS Software (Aspekto)/Hybrid SaaS Software/Website/Core/Website/Script/Product-detail.ts" />
 
 if ($('.sheet')) {
-    alert("test");
+   
 };
 
 $(() => {
