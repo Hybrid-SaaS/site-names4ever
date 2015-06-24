a68e01304673676a82390992fac557bffc03d9b1
Randydedecker <randydedecker@gmail.com>
2015-06-24 09:56:30 +0200

    alert werkt

--- pages/product-detail/product-detail.js
+++ pages/product-detail/product-detail.js
@@ -1,7 +1,6 @@
 /// <reference path="../../definition/jquery.d.ts" />
 /// <reference path="../../../../../Hybrid SaaS/Hybrid SaaS Software (Aspekto)/Hybrid SaaS Software/Website/Core/Website/Script/Product-detail.ts" />
 if ($('.sheet')) {
-    alert("test");
     $('.sheet').html('');
 }
 
