8409ab9573be92c60b86e83d6fcedb6e1bfd762c
Randydedecker <randydedecker@gmail.com>
2015-12-08 17:09:45 +0100

    menu javascript

---
+++ header/names4ever/script/menu.ts
@@ -0,0 +1,67 @@
+﻿$(document).ready(function () {
+    var gaverder = true;
+    var Click = function () {
+        var gaverder = true;
+        event.stopPropagation();
+        event.preventDefault();
+        if ($(this).hasClass('active')) {
+            gaverder = false;
+        }
+        // Een ander menu item staat open
+        if (gaverder) {
+            // Vouwt openstaande dicht       
+            $('.drop-down-clickme.active > ul').slideUp(0);
+            $('.drop-down-clickme.active').removeClass('active');
+            // Vouwt deze open
+            if ($(this).children('ul').is(':visible') == false) {
+                $(this).addClass('active');
+                $(this).children('ul').slideToggle(0);
+            }
+
+        }
+        // Hetzelfde menuitem dicht
+        else {
+            $(this).children('ul').slideToggle(0);
+            $('.drop-down-clickme.active').removeClass('active');
+        }
+        //Verander gaverder variabele waardoor volgende functie niet uitgevoerd wordt
+        gaverder = false;
+    };
+
+    var Hover = function () {
+        var gaverder = true
+	if ($(this).hasClass('active')) {
+            gaverder = false;
+        }
+        // Huidige hover staat nog niet open
+        if (gaverder) {
+            // Vouwt openstaande dicht       
+            $('.drop-down-clickme.active > ul').slideUp(0);
+            $('.drop-down-clickme.active').removeClass('active');
+            // Vouwt deze open
+            if ($(this).children('ul').is(':visible') == false) {
+                $(this).addClass('active');
+                $(this).children('ul').slideToggle(0);
+            }
+
+        }
+        //Verander gaverder variabele waardoor volgende functie niet uitgevoerd wordt
+        gaverder = false;
+    };
+
+    $(".drop-down-clickme").on("click touchend", Click);
+    $(".drop-down-clickme").hover(Hover);
+
+    // Vouwt de openstaande dicht als ergens in de body geklikt wordt
+    $("html").bind("click touchend", function () {
+        if (gaverder) {
+            $('.drop-down-clickme.active > ul').slideUp(0);
+            $('.drop-down-clickme.active').removeClass('active');
+        }
+        gaverder = true;
+    });
+    // Behalve als er in het openstaande menu geklikt wordt
+    $('.drop-down-clickme > ul').bind("click touchend", function () {
+        event.stopPropagation();
+    });
+});
