74c1752ea236378829e2e244828f384d15f4cf50
Randydedecker <randydedecker@gmail.com>
2015-12-11 14:33:58 +0100

    update menu

--- header/names4ever/script/menu.js
+++ header/names4ever/script/menu.js
@@ -1,5 +1,6 @@
 ﻿$(document).ready(function () {
     var gaverder = true;
+
     var Click = function () {
         var gaverder = true;
         event.stopPropagation();
@@ -7,62 +8,84 @@
         if ($(this).hasClass('active')) {
             gaverder = false;
         }
-
         // Een ander menu item staat open
         if (gaverder) {
-            // Vouwt openstaande dicht
-            $('.drop-down-clickme.active > ul').slideUp(0);
+            // Vouwt openstaande dicht       
+            $('.drop-down-clickme.active > ul').hide();
             $('.drop-down-clickme.active').removeClass('active');
-
             // Vouwt deze open
             if ($(this).children('ul').is(':visible') == false) {
                 $(this).addClass('active');
-                $(this).children('ul').slideToggle(0);
+                $(this).children('ul').fadeIn("fast").show();
             }
-        } else {
-            $(this).children('ul').slideToggle(0);
-            $('.drop-down-clickme.active').removeClass('active');
-        }
 
+        }
+            // Hetzelfde menuitem dicht behalve bij hover
+        else {
+            if (!$(this).hasClass('disabled')) {
+                $(this).children('ul').hide();
+                $('.drop-down-clickme.active').removeClass('active');
+            }
+        }
         //Verander gaverder variabele waardoor volgende functie niet uitgevoerd wordt
         gaverder = false;
     };
 
-    var Hover = function () {
+    var Flyout = function () {
         var gaverder = true;
         if ($(this).hasClass('active')) {
             gaverder = false;
         }
-
         // Huidige hover staat nog niet open
         if (gaverder) {
-            // Vouwt openstaande dicht
-            $('.drop-down-clickme.active > ul').slideUp(0);
+            // Vouwt openstaande dicht       
+            $('.drop-down-clickme.active > ul').hide();
             $('.drop-down-clickme.active').removeClass('active');
+            $('.drop-down-clickme').removeClass('disabled');
 
             // Vouwt deze open
             if ($(this).children('ul').is(':visible') == false) {
                 $(this).addClass('active');
-                $(this).children('ul').slideToggle(0);
+                $(this).children('ul').fadeIn("fast").show();
+                $(this).addClass('disabled');
+                setTimeout(function () {
+                    // enable click after 1 second
+                    $('.drop-down-clickme.disabled').removeClass('disabled');
+                }, 3000);
             }
         }
-
         //Verander gaverder variabele waardoor volgende functie niet uitgevoerd wordt
         gaverder = false;
     };
 
+    var Flyin = function () {
+        var gaverder = true;
+        // Huidige hover staat nog niet open
+        if (gaverder) {
+            // Vouwt openstaande dicht       
+            $('.drop-down-clickme.active > ul').hide();
+            $('.drop-down-clickme.active').removeClass('active');
+        }
+        //Verander gaverder variabele waardoor volgende functie niet uitgevoerd wordt
+        gaverder = false;
+    };
     $(".drop-down-clickme").on("click touchend", Click);
-    $(".drop-down-clickme").hover(Hover);
+    $(".drop-down-clickme").hoverIntent({
+        over: Flyout,
+        out: Flyin,
+        timeout: 100,
+        interval: 100
 
+    });
     // Vouwt de openstaande dicht als ergens in de body geklikt wordt
     $("html").bind("click touchend", function () {
+        event.stopPropagation();
         if (gaverder) {
             $('.drop-down-clickme.active > ul').slideUp(0);
             $('.drop-down-clickme.active').removeClass('active');
         }
         gaverder = true;
     });
-
     // Behalve als er in het openstaande menu geklikt wordt
     $('.drop-down-clickme > ul').bind("click touchend", function () {
         event.stopPropagation();
