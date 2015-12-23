f44600eae50496cc42f61559a8bda69774c23549
Randydedecker <randydedecker@gmail.com>
2015-12-23 16:46:28 +0100

    populaire producten zoals DOTW

--- pages/home/home_new.js
+++ pages/home/home_new.js
@@ -21,15 +21,69 @@ $(function () {
     var currentIndex = $slides.length - 1;
 
     
-    //
+    // Top category
     $('#topcat').append($('#topcategory'));
 
+
+    // Deal of the week
     $.getJSON('/data/product/DEALOFTHEDAY/related-products').done(function (data) {
         var $deal = $('#deal');
         $('#deal').append('<div class="head">Deal of the Week</div>');
 
         if (data.related && data.related["Deal"]) {
             var product = data.related["Deal"][0];
+            var $img = $('<img src="/image/product/guid/' + encodeURIComponent(product.guid) + '?width=175&height=175" />');
+            $img.attr({ 'title': product.productcode + '\n' + product.description });
+            $deal.data('url', product.url);
+
+            $deal.append($img);
+
+            $deal.append($('<div class="title"></div>').text(product.description));
+
+            var from = '';
+            var to = '';
+            switch (WebPage.Data.language) {
+                case 'nl': {
+                    from = 'Van';
+                    to = 'Voor';
+                    break;
+                }
+                case 'de': {
+                    from = 'Bisher';
+                    to = 'Jetzt';
+                    break;
+                }
+                case 'en': {
+                    from = 'From';
+                    to = 'Now';
+                    break;
+                }
+            }
+
+            if (WebPage.Data.country == 'gb') {
+                $deal.append($('<div class="price-original"></div>').text(from + ' £ ' + product['price-original'].toFixed(2).replace('.', ',')));
+                $deal.append($('<div class="price"></div>').text(to + ' £ ' + product.price.toFixed(2).replace('.', ',')));
+            } else {
+                $deal.append($('<div class="price-original"></div>').text(from + ' € ' + product['price-original'].toFixed(2).replace('.', ',')));
+                $deal.append($('<div class="price"></div>').text(to + ' € ' + product.price.toFixed(2).replace('.', ',')));
+            }
+            $deal.attr({ 'title': product.productcode + '\n' + product.description });
+
+            $deal.click(function () {
+                location.href = $deal.data('url');
+            });
+
+            $deal.fadeIn(750);
+        }
+    });
+
+    // Populaire producten Home
+    $.getJSON('/data/product/POPULAIRHOME/related-products').done(function (data) {
+        var $deal = $('#populair');
+        $('#populair').append('<div class="head">Populair Home</div>');
+
+        if (data.related && data.related["Populair"]) {
+            var product = data.related["Populair"][0];
             var $img = $('<img src="/image/product/guid/' + encodeURIComponent(product.guid) + '?width=175&height=175" />');
             $img.attr({ 'title': product.productcode + '\n' + product.description });
             $deal.data('url', product.url);
