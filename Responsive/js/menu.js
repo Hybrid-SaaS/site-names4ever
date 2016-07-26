20a0633e3a9714d8e7729a02b19eeee6485e54a8
Randydedecker <randydedecker@gmail.com>
2016-07-26 16:08:36 +0200

    test

--- Responsive/js/menu.js
+++ Responsive/js/menu.js
@@ -1,30 +1,30 @@
-﻿$(document).ready(function () {
+﻿jQuery(document).ready(function () {
     var gaverder = true;
 
     var Click = function () {        
         var gaverder = true;
         event.stopPropagation();
         event.preventDefault();
-        if ($(this).hasClass('active')) {
+        if (jQuery(this).hasClass('active')) {
             gaverder = false;
         }
         // Een ander menu item staat open
         if (gaverder) {
             // Vouwt openstaande dicht       
-            $('.drop-down-clickme.active > ul').hide();
-            $('.drop-down-clickme.active').removeClass('active');
+            jQuery('.drop-down-clickme.active > ul').hide();
+            jQuery('.drop-down-clickme.active').removeClass('active');
             // Vouwt deze open
-            if ($(this).children('ul').is(':visible') == false) {
-                $(this).addClass('active');
-                $(this).children('ul').fadeIn("fast").show();
+            if (jQuery(this).children('ul').is(':visible') == false) {
+                jQuery(this).addClass('active');
+                jQuery(this).children('ul').fadeIn("fast").show();
             }
 
         }
             // Hetzelfde menuitem dicht behalve bij hover
         else {
-            if (!$(this).hasClass('disabled')) {
-                $(this).children('ul').hide();
-                $('.drop-down-clickme.active').removeClass('active');
+            if (!jQuery(this).hasClass('disabled')) {
+                jQuery(this).children('ul').hide();
+                jQuery('.drop-down-clickme.active').removeClass('active');
             }
         }
         //Verander gaverder variabele waardoor volgende functie niet uitgevoerd wordt
@@ -33,21 +33,21 @@
 
     var Flyout = function () {
         var gaverder = true;
-        if ($(this).hasClass('active')) {
+        if (jQuery(this).hasClass('active')) {
             gaverder = false;
         }
         // Huidige hover staat nog niet open
         if (gaverder) {
             // Vouwt openstaande dicht       
-            $('.drop-down-clickme.active > ul').hide();
-            $('.drop-down-clickme.active').removeClass('active');
-            $('.drop-down-clickme').removeClass('disabled');
+            jQuery('.drop-down-clickme.active > ul').hide();
+            jQuery('.drop-down-clickme.active').removeClass('active');
+            jQuery('.drop-down-clickme').removeClass('disabled');
 
             // Vouwt deze open
-            if ($(this).children('ul').is(':visible') == false) {
-                $(this).addClass('active');
-                $(this).children('ul').fadeIn("fast").show();
-                $(this).addClass('disabled');
+            if (jQuery(this).children('ul').is(':visible') == false) {
+                jQuery(this).addClass('active');
+                jQuery(this).children('ul').fadeIn("fast").show();
+                jQuery(this).addClass('disabled');
                 
             }
         }
@@ -60,14 +60,14 @@
         // Huidige hover staat nog niet open
         if (gaverder) {
             // Vouwt openstaande dicht       
-            $('.drop-down-clickme.active > ul').hide();
-            $('.drop-down-clickme.active').removeClass('active');
+            jQuery('.drop-down-clickme.active > ul').hide();
+            jQuery('.drop-down-clickme.active').removeClass('active');
         }
         //Verander gaverder variabele waardoor volgende functie niet uitgevoerd wordt
         gaverder = false;
     };
-    $(".drop-down-clickme").on("click touchend", Click);
-    $(".drop-down-clickme").hoverIntent({
+    jQuery(".drop-down-clickme").on("click touchend", Click);
+    jQuery(".drop-down-clickme").hoverIntent({
         over: Flyout,
         out: Flyin,
         timeout: 100,
@@ -75,16 +75,16 @@
 
     });
     // Vouwt de openstaande dicht als ergens in de body geklikt wordt
-    $("html").bind("click touchend", function () {
+    jQuery("html").bind("click touchend", function () {
         event.stopPropagation();
         if (gaverder) {
-            $('.drop-down-clickme.active > ul').slideUp(0);
-            $('.drop-down-clickme.active').removeClass('active');
+            jQuery('.drop-down-clickme.active > ul').slideUp(0);
+            jQuery('.drop-down-clickme.active').removeClass('active');
         }
         gaverder = true;
     });
     // Behalve als er in het openstaande menu geklikt wordt
-    $('.drop-down-clickme > ul').bind("click touchend", function () {
+    jQuery('.drop-down-clickme > ul').bind("click touchend", function () {
         event.stopPropagation();
     });
 });
